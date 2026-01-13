76b2d812df9bfb953c7034b84c3565e2651a0905
pneumatick <jhwwefan@gmail.com>
2026-01-13 10:37:39 -0500

    Read messages from clients until the client closes their connection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
 type Error = Box<dyn std::error::Error>; 
 type Result<T> = std::result::Result<T, Error>; 
 
-fn handle_client(stream: TcpStream) -> Result<()> {
-    let result = stream.shutdown(Shutdown::Both);
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    let mut buffer = [0; 512];
 
-    Ok(result?)
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+
+        // Handle incoming data
+        if bytes_read != 0 {
+            println!("Received {} bytes: {:?}", bytes_read, &buffer[..bytes_read]);
+
+            // If the data is expected to be a UTF-8 string, you can convert it.
+            // Be cautious as TCP streams can contain arbitrary bytes.
+            match std::str::from_utf8(&buffer[..bytes_read]) {
+                Ok(message) => println!("Message: {}", message),
+                Err(e) => eprintln!("Could not parse as utf8: {}", e),
+            }
+        } 
+        // Handle client disconnect
+        else {
+            break;
+        }
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
